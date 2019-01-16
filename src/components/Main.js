23cd0926d4e1eecc73a1cf3dc81a6e151d7c42bf
Masen Matthews <masenmatthews@gmail.com>
2019-01-16 15:53:30 -0800

    add live preview for weather app

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,7 @@ class Main extends React.Component {
 
         <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
-          <span className="image main"><img src={masen} alt="picture of Masen Matthews" /></span>
+          <span className="image main"><img src={masen} alt="Masen Matthews" /></span>
             <p>I'm a web developer based in Portland, Oregon. My background is primarily rooted in JavaScript, Ruby, and Python. I also have experience using technologies like Git, Ruby On Rails, React, Angular, and SQL. I am constantly adding to this list and love embracing oppurtunities to learn about new languages, frameworks, and development tools. If you're interested in checking out my latest coding endeavors, feel free to check out my <a href="https://github.com/masenmatthews">GitHub</a></p>
 
             <p>My main passion for web development lies in the realm of accessibility. I want to make the web a more inclusive place for people with disabilities and I ultimately dream of finding a place in the tech industry that allows me to help disabled individuals succeed with technology. Similarly, I'm passionate about using technology as a means of fostering social change, sustainability, and socioeconomic mobility for those in need.</p>
@@ -28,7 +28,7 @@ class Main extends React.Component {
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work</h2>
-          <span className="image main"><img src={code} alt="Black and white photo of a laptop with coding window open on the display." /></span>
+          <span className="image main"><img src={code} alt="Laptop with coding window open on the display." /></span>
           <h3>AccessiReference</h3>
           <h4><a href="https://github.com/masenmatthews/AccessiReference">Repository |</a><a href="https://practical-nightingale-ecd7dc.netlify.com/"> Live Preview</a></h4>
           <p>AccessiReference is designed to act as a simple, convienent reference for accessibility functionality. The goal of this app is to make it easy for developers to learn more about accessible design choices so that they can practice adding accessibility features to their applications. This application is currently in progress. It is being completed while I work through Deque University's online web accessibility curriculum.</p>
@@ -42,7 +42,7 @@ class Main extends React.Component {
           <p>This is a collaborative study app that was made with Angular 2. The app has blog, forum, and live chat features to help collaborators succeed. It also possesses authentication functionality, hosting and database features through Firebase, and Materialize styling.</p>
 
           <h3>React Weather App</h3>
-          <h4><a href="https://github.com/masenmatthews/React-Weather-App">Repository |</a><a href="#"> Live Preview Coming Soon</a></h4>
+          <h4><a href="https://github.com/masenmatthews/React-Weather-App">Repository |</a><a href="https://agitated-franklin-55a50d.netlify.com/"> Live Preview</a></h4>
           <p>This is a simple weather app that was built with React. It pulls weather data from the OpenWeather API when the user enters a city and the country that it is in. This project has helped me learn more about React, state, and pulling data from APIs.</p>
 
           <h3>More projects coming soon!</h3>
@@ -52,7 +52,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
         <h2 className="major">Contact</h2>
-        <span className="image main"><img src={letter} alt="Black and white photo of a laptop with coding window open on the display." /></span>
+        <span className="image main"><img src={letter} alt="Pen on paper" /></span>
           <p>Interested in connecting? I can be reached at masenmatthews@gmail.com. Alternatively, feel free to reach out on Twitter, GitHub, or LinkedIn using the buttons below.
           </p>
           <ul className="icons">
